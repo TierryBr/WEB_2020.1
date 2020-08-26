b42c63668415eda27149a6060cdc7455fea55604
TierryBr <tierri-2011@hotmail.com>
2020-08-26 19:48:40 -0300

    atividade 05

---
+++ atividade05/models/DisciplinasModel.js
@@ -0,0 +1,10 @@
+class DisciplinasModel {
+    constructor(_id, nome, curso, capacidade) {
+        this._id = _id
+        this.nome = nome
+        this.curso = curso
+        this.capacidade = capacidade
+    }
+}
+
+module.exports = DisciplinasModel
